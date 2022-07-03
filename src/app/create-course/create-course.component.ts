ac1a3deeae1aee9e3d5c774878d26f4bb3d2718d
javierpb275 <javierpb275@gmail.com>
2022-07-03 16:24:33 +0200

    create course form

--- src/app/create-course/create-course.component.ts
+++ src/app/create-course/create-course.component.ts
@@ -1,27 +1,36 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {AngularFirestore} from '@angular/fire/firestore';
-import {Course} from '../model/course';
-import {catchError, concatMap, last, map, take, tap} from 'rxjs/operators';
-import {from, Observable, throwError} from 'rxjs';
-import {Router} from '@angular/router';
-import {AngularFireStorage} from '@angular/fire/storage';
-import firebase from 'firebase/app';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { Course } from "../model/course";
+import { catchError, concatMap, last, map, take, tap } from "rxjs/operators";
+import { from, Observable, throwError } from "rxjs";
+import { Router } from "@angular/router";
+import { AngularFireStorage } from "@angular/fire/storage";
+import firebase from "firebase/app";
 import Timestamp = firebase.firestore.Timestamp;
 
 @Component({
-  selector: 'create-course',
-  templateUrl: 'create-course.component.html',
-  styleUrls: ['create-course.component.css']
+  selector: "create-course",
+  templateUrl: "create-course.component.html",
+  styleUrls: ["create-course.component.css"],
 })
 export class CreateCourseComponent implements OnInit {
+  form = this.fb.group({
+    description: ["", Validators.required],
+    category: ["BEGINNER", Validators.required],
+    url: ["", Validators.required],
+    longDescription: ["", Validators.required],
+    promo: [false],
+    promoStartAt: [null],
+  });
 
-  constructor() {
+  constructor(private fb: FormBuilder) {}
 
+  ngOnInit() {}
+
+  onCreateCourse() {
+    const newCourse = {...this.form.value} as Course;
+    newCourse.promoStartAt = Timestamp.fromDate(this.form.value.promoStartAt);
+    console.log(newCourse);
   }
-
-  ngOnInit() {
-
-  }
-
 }
