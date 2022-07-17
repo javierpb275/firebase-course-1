fb7592eae68f23d15684aa52927c6288d2981dd5
javierpb275 <javierpb275@gmail.com>
2022-07-17 22:05:07 +0200

    firebase cloud functions . intro to db triggers

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,14 @@ import * as functions from "firebase-functions";
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+export const onAddCourseUpdatePromoCounter = functions
+  .runWith({
+    timeoutSeconds: 300,
+    memory: "128MB",
+  })
+  .firestore.document("courses/{courseId}")
+  .onCreate(async (snap, context) => {
+    functions.logger.debug(
+      `Running add course trigger for courseId ${context.params.courseId}`
+    );
+  });
