32184ede7254481b4db1867b5cf9d7488b268000
javierpb275 <javierpb275@gmail.com>
2022-07-22 21:15:07 +0200

    calling a firebase cloud function rest endpoint

--- functions/src/create-user.ts
+++ functions/src/create-user.ts
@@ -1,14 +1,22 @@
 const express = require("express");
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 export const createUserApp = express();
 
-createUserApp.post("/", async (req, res)  => {
- functions.logger.debug(`Calling create user function.`);
- try {
-    res.status(200).json({message: "User created successfully"});
- } catch (err) {
+createUserApp.use(bodyParser.json());
+createUserApp.use(cors({ origin: true }));
+
+createUserApp.post("/", async (req, res) => {
+  functions.logger.debug(`Calling create user function.`);
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+    const admin = req.body.admin;
+    res.status(200).json({ message: "User created successfully" });
+  } catch (err) {
     functions.logger.error(`Could not create user.`, err);
-    res.status(500).json({message: "Could not create user."});
- }
-})
+    res.status(500).json({ message: "Could not create user." });
+  }
+});
