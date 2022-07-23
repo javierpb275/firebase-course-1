a4ddcbb7066365b1dfbfe065f32644333c84d1a2
javierpb275 <javierpb275@gmail.com>
2022-07-23 13:31:14 +0200

    set up angular http interceptor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,23 @@
-import {Component, OnInit} from '@angular/core';
-import {AngularFireAuth} from '@angular/fire/auth';
-import {from, Observable} from 'rxjs';
-import {concatMap, filter, map} from 'rxjs/operators';
-import {AngularFirestore} from '@angular/fire/firestore';
-import {Router} from '@angular/router';
-import { UserService } from './services/user.service';
+import { Component, OnInit } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { from, Observable } from "rxjs";
+import { concatMap, filter, map } from "rxjs/operators";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { Router } from "@angular/router";
+import { UserService } from "./services/user.service";
+import { AuthTokenService } from "./services/auth-token.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
+  constructor(public user: UserService, private token: AuthTokenService) {}
 
-  constructor(public user: UserService) {
-
-  }
-
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   logout() {
-    this.user.logout()
+    this.user.logout();
   }
-
 }
